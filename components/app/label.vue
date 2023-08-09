bb5e26d519dfe0fb0d7519c62d7f1e380ff5b055
agfebrian <febriananggi9@gmail.com>
2023-08-09 11:16:26 +0700

    add: component label

---
+++ components/app/label.vue
@@ -0,0 +1,23 @@
+<template>
+  <label :for="to" :class="[error, 'font-normal']">
+    {{ text }}
+  </label>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  to?: string;
+  text: string;
+  error?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  to: "",
+  error: "",
+});
+
+const error = computed(() => {
+  if (props.error) return "text-danger-default";
+  else return "text-grey-dark";
+});
+</script>
