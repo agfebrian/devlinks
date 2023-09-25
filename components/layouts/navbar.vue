5ecd53631cf503e742fabe76eb76ec42e7b64d0e
agfebrian <febriananggi9@gmail.com>
2023-09-25 07:02:50 +0700

    add: component layout

---
+++ components/layouts/navbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <header
+    :class="[
+      background == 'default' ? 'bg-white' : 'bg-primary-default',
+      'sticky top-0 z-30 p-6',
+    ]"
+  >
+    <div
+      class="flex items-center justify-between rounded-xl bg-white px-6 py-4"
+    >
+      <icon-devlinks />
+      <div class="flex gap-4">
+        <app-button
+          v-for="(item, index) in navigation"
+          :key="index"
+          class="flex items-center gap-2"
+          variant="ghosted"
+          :active="activeNav === item.link"
+          @mouseover="item.color = 'primary'"
+          @mouseleave="
+            activeNav === item.link ? null : (item.color = 'default')
+          "
+          @click="navStore.changeNav(item.link)"
+        >
+          <icon-link v-if="item.link == '/links'" :color="item.color" />
+          <icon-profile v-else :color="item.color" />
+          {{ item.text }}
+        </app-button>
+      </div>
+      <app-button variant="outlined">Preview</app-button>
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts">
+import { storeToRefs } from "pinia";
+import { useNavigation } from "@/store/use-navigation";
+
+interface Props {
+  background?: "default" | "primary";
+}
+
+withDefaults(defineProps<Props>(), { background: "default" });
+
+const navStore = useNavigation();
+const { navigation, activeNav } = storeToRefs(navStore);
+</script>
